b84888837b79b93321f99dd28798a917c811515e
Marko351 <markodjuric351@gmail.com>
2023-01-19 21:37:04 +0100

    add reaction to DB

---
+++ backend/src/shared/services/db/reaction.service.ts
@@ -0,0 +1,24 @@
+import { IPostDocument } from '@post/interfaces/post.interface';
+import { PostModel } from '@post/models/post.schema';
+import { IReactionDocument, IReactionJob } from '@reaction/interfaces/reaction.interface';
+import { ReactionModel } from '@reaction/model/reaction.schema';
+import { UserCache } from '@service/redis/user.cache';
+import { IUserDocument } from '@user/interfaces/user.interface';
+
+const userCache: UserCache = new UserCache();
+
+class ReactionService {
+  public async addReactionDataToDB(reactionData: IReactionJob): Promise<void> {
+    const { postId, username, userFrom, userTo, type, previousReaction, reactionObject } = reactionData;
+
+    const updatedReaction: [IUserDocument, IReactionDocument, IPostDocument] = (await Promise.all([
+      userCache.getUserFromCache(`${userTo}`),
+      ReactionModel.replaceOne({ postId, type: previousReaction, username }, reactionObject, { upsert: true }),
+      PostModel.findOneAndUpdate({ _id: postId }, { $inc: { [`reactions.${previousReaction}`]: -1, [`reactions.${type}`]: 1 } }, { new: true })
+    ])) as unknown as [IUserDocument, IReactionDocument, IPostDocument];
+
+    // send reactions notification
+  }
+}
+
+export const reactionService: ReactionService = new ReactionService();
