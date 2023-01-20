47d810dd6a796a76af5103702b17ab2737e30d9e
Marko351 <markodjuric351@gmail.com>
2023-01-20 10:40:21 +0100

    remove reaction controller

--- backend/src/features/reaction/routes/reaction.routes.ts
+++ backend/src/features/reaction/routes/reaction.routes.ts
@@ -1,5 +1,6 @@
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { AddReactions } from '@reaction/controllers/add-reactions.controller';
+import { RemoveReactions } from '@reaction/controllers/remove-reactions.controller';
 import { Router } from 'express';
 
 class ReactionRoutes {
@@ -11,6 +12,12 @@ class ReactionRoutes {
 
   public routes(): Router {
     this.router.post('/post/reaction', authMiddleware.checkAuthentication, AddReactions.prototype.reaction);
+
+    this.router.delete(
+      '/post/reaction/:postId/:previousReaction/:postReactions',
+      authMiddleware.checkAuthentication,
+      RemoveReactions.prototype.reaction
+    );
 
     return this.router;
   }
