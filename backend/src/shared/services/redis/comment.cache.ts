8be3ef4b50e538a101999ae0fb98263dd4c1e47f
Marko351 <markodjuric351@gmail.com>
2023-01-22 14:02:31 +0100

    get comment names and single comment from cache

--- backend/src/shared/services/redis/comment.cache.ts
+++ backend/src/shared/services/redis/comment.cache.ts
@@ -5,7 +5,7 @@ import { config } from '@root/config';
 import { ServerError } from '@global/helpers/error-handler';
 import { IReactionDocument, IReactions } from '@reaction/interfaces/reaction.interface';
 import { Helpers } from '@global/helpers/helpers';
-import { ICommentDocument } from '@comment/interfaces/comment.interface';
+import { ICommentDocument, ICommentNameList } from '@comment/interfaces/comment.interface';
 
 const log: Logger = config.createLogger('commentsCache');
 
@@ -33,7 +33,7 @@ export class CommentCache extends BaseCache {
     }
   }
 
-  public async getCommentsToCache(postId: string, value: string): Promise<ICommentDocument[]> {
+  public async getCommentsFromCache(postId: string): Promise<ICommentDocument[]> {
     try {
       if (!this.client.isOpen) {
         await this.client.connect();
@@ -47,6 +47,55 @@ export class CommentCache extends BaseCache {
       }
 
       return comments;
+    } catch (err) {
+      log.error(err);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+  public async getCommentNamesFromCache(postId: string): Promise<ICommentNameList[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const commentsLength = await this.client.LLEN(`comments:${postId}`);
+      const comments: string[] = await this.client.LRANGE(`comments:${postId}`, 0, -1);
+      const list: string[] = [];
+      for (const item of comments) {
+        const comment: ICommentDocument = Helpers.parseJson(item) as ICommentDocument;
+        list.push(comment.username);
+      }
+
+      const response: ICommentNameList = {
+        count: commentsLength,
+        names: list
+      };
+
+      return [response];
+    } catch (err) {
+      log.error(err);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+  public async getSingleCommentFromCache(postId: string, commentId: string): Promise<ICommentDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const comments: string[] = await this.client.LRANGE(`comments:${postId}`, 0, -1);
+      const list: ICommentDocument[] = [];
+      for (const item of comments) {
+        list.push(Helpers.parseJson(item));
+      }
+
+      const result: ICommentDocument = find(list, (listItem: ICommentDocument) => {
+        return listItem._id === commentId;
+      }) as ICommentDocument;
+
+      return [result];
     } catch (err) {
       log.error(err);
       throw new ServerError('Server error. Try again.');
