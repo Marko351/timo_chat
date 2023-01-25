5827680599d54c10e0a354fb2b40f651bcc89a4b
Marko351 <markodjuric351@gmail.com>
2023-01-25 10:03:45 +0100

    get followers controller

--- backend/src/features/followers/routes/follower.routes.ts
+++ backend/src/features/followers/routes/follower.routes.ts
@@ -1,4 +1,5 @@
 import { Add } from '@followers/controllers/follow-user.controller';
+import { Get } from '@followers/controllers/get-followers.controller';
 import { Remove } from '@followers/controllers/unfollow-user.controller';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Router } from 'express';
@@ -13,6 +14,9 @@ class FollowerRoutes {
   public routes(): Router {
     this.router.put('/user/follow/:followerId', authMiddleware.checkAuthentication, Add.prototype.follower);
     this.router.put('/user/unfollow/:followeeId/:followerId', authMiddleware.checkAuthentication, Remove.prototype.follower);
+
+    this.router.get('/user/following', authMiddleware.checkAuthentication, Get.prototype.userFollowing);
+    this.router.get('/user/followers/:userId', authMiddleware.checkAuthentication, Get.prototype.userFollowers);
 
     return this.router;
   }
