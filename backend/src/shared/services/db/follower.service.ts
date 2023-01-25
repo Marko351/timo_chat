d020da97162c87f4e204d72b0a6716a8aab4240d
Marko351 <markodjuric351@gmail.com>
2023-01-25 09:27:12 +0100

    get follower and followee data from db

--- backend/src/shared/services/db/follower.service.ts
+++ backend/src/shared/services/db/follower.service.ts
@@ -1,4 +1,4 @@
-import { IFollowerDocument } from '@followers/interfaces/followers.interfaces';
+import { IFollowerData, IFollowerDocument } from '@followers/interfaces/followers.interfaces';
 import { FollowerModel } from '@followers/models/followers.scheme';
 import { IQueryComplete, IQueryDeleted } from '@post/interfaces/post.interface';
 import { UserModel } from '@user/models/user.schema';
@@ -58,6 +58,72 @@ class FollowerService {
     ]);
 
     await Promise.all([users, unfollow]);
+  }
+
+  public async getFolloweeData(userObjectId: ObjectId): Promise<IFollowerData[]> {
+    const followee = await FollowerModel.aggregate([
+      { $match: { followerId: userObjectId } },
+      { $lookup: { from: 'User', localField: 'followeeId', foreignField: '_id', as: 'followeeId' } },
+      { $unwind: '$followeeId' },
+      { $lookup: { from: 'Auth', localField: 'followeeId.authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      {
+        $addFields: {
+          _id: '$followeeId._id',
+          username: '$authId.username',
+          avatarColor: '$authId.avatarColor',
+          postCount: '$followeeId.postsCount',
+          followersCount: '$followeeId.followersCount',
+          followingCount: '$followeeId.followingCount',
+          profilePicture: '$followeeId.profilePicture',
+          userProfile: '$followeeId'
+        }
+      },
+      {
+        $project: {
+          authId: 0,
+          followerId: 0,
+          followeeId: 0,
+          createdAt: 0,
+          __v: 0
+        }
+      }
+    ]);
+
+    return followee;
+  }
+
+  public async getFollowerData(userObjectId: ObjectId): Promise<IFollowerData[]> {
+    const follower = await FollowerModel.aggregate([
+      { $match: { followeeId: userObjectId } },
+      { $lookup: { from: 'User', localField: 'followerId', foreignField: '_id', as: 'followerId' } },
+      { $unwind: '$followerId' },
+      { $lookup: { from: 'Auth', localField: 'followerId.authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      {
+        $addFields: {
+          _id: '$followerId._id',
+          username: '$authId.username',
+          avatarColor: '$authId.avatarColor',
+          postCount: '$followerId.postsCount',
+          followersCount: '$followerId.followersCount',
+          followingCount: '$followerId.followingCount',
+          profilePicture: '$followerId.profilePicture',
+          userProfile: '$followerId'
+        }
+      },
+      {
+        $project: {
+          authId: 0,
+          followerId: 0,
+          followeeId: 0,
+          createdAt: 0,
+          __v: 0
+        }
+      }
+    ]);
+
+    return follower;
   }
 }
 
