b22bcce898a81c3fb6577efe971ee3bd55e4b1ab
Marko351 <markodjuric351@gmail.com>
2023-01-27 17:46:44 +0100

    notification schema and inerfaces

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -28,6 +28,7 @@ const config: Config.InitialOptions = {
     '@reaction/(.*)': ['<rootDir>/src/features/reaction/$1'],
     '@comment/(.*)': ['<rootDir>/src/features/comment/$1'],
     '@followers/(.*)': ['<rootDir>/src/features/followers/$1'],
+    '@notifications/(.*)': ['<rootDir>/src/features/notifications/$1'],
     '@global/(.*)': ['<rootDir>/src/shared/globals/$1'],
     '@service/(.*)': ['<rootDir>/src/shared/services/$1'],
     '@socket/(.*)': ['<rootDir>/src/shared/sockets/$1'],
